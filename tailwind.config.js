2921f397503298dfe14c17e223c9857d01a4c7b9
DoungChan <sreoun.doungchan19@kit.edu.kh>
2023-05-30 13:59:19 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+delete colors["lightBlue"];
+delete colors["warmGray"];
+delete colors["trueGray"];
+delete colors["coolGray"];
+delete colors["blueGray"];
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    fontfamily: {
+      open_sans: ["Open Sans", "sans-serif"],
+    },
+    colors: {
+      ...colors,
+      ...{
+        primary: "#6F47EB",
+        secondary: "#FFFFFF",
+        font_color: "#1E1926",
       },
     },
   },
   plugins: [],
-}
+};
