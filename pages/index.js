d0a528d7aa599718204926c2463552f73d6e6179
Oun Visidhreach <60146263+OunVisidhreach@users.noreply.github.com>
2023-06-05 13:09:01 +0700

    Reach dev (#14)
    * Update README.md
    
    * unauthorized login
    
    * unauthorize and login screen
    
    * change folder
    
    * change folder
    
    ---------
    
    Co-authored-by: DoungChan <63252991+DoungChan@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import UnauthorizeComponent from "@/components/unauthorized/UnauthorizeComponent";
 import Navbar from "../components/navbar/Navbar";
-
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between `}
-    ></main>
+    >
+      <UnauthorizeComponent />
+    </main>
   );
 }
