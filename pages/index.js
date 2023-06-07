eb6cc6a86279b9d66f8dc63139d812cee4d8ea2c
siii123 <58299082+SivEng@users.noreply.github.com>
2023-06-07 14:08:29 +0700

    category/feature-promotion (#22)
    * category/feature-promotion
    
    * add responsive
    
    ---------
    
    Co-authored-by: sivengo <sivengorn@gmail.com>
    Co-authored-by: DoungChan <63252991+DoungChan@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import PopularPromotions from "@/components/popular/PopularPromotions";
 import Navbar from "../components/navbar/Navbar";
 import UnauthorizeComponent from "@/components/unauthorized/UnauthorizeComponent";
+import Category from "../components/category-feature/Categories"
+import HotPromotion from "../components/category-feature/Feature"
+import Feature from "../components/category-feature/Feature";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between `}
-    >
+
+    <main className={`flex min-h-screen flex-col items-center justify-between `}>
+      <Feature />
+      <Category />
+
+   
       <PopularPromotions />
+
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,10 @@ module.exports = {
         secondary: "#FFFFFF",
         font_color: "#1E1926",
         sub_font_color: "#5E5D6D",
-        lightblue: "#bfe8ff",
+
+        lightBlue: "#BFE8FF",
+        blue: "#005EAB",
+
       },
     },
   },
