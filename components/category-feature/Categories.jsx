29c8d5e4312010792b76f8d723e206043b097d78
siii123 <58299082+SivEng@users.noreply.github.com>
2023-06-14 14:29:08 +0700

    change categories & Feature (#38)
    Co-authored-by: sivengo <sivengorn@gmail.com>

--- components/category-feature/Categories.jsx
+++ components/category-feature/Categories.jsx
@@ -1,75 +1,34 @@
+import Link from "next/link";
 import Image from "next/image";
-import category1 from "public/Categories/category1.png";
-import category2 from "public/Categories/category2.png";
-import category3 from "public/Categories/category3.png";
-import category4 from "public/Categories/category4.png";
-import category5 from "public/Categories/category5.png";
-import category6 from "public/Categories/category6.png";
 
+const categories = [
+  { id: 1, imageSrc: "/Categories/category1.png" },
+  { id: 2, imageSrc: "/Categories/category2.png" },
+  { id: 3, imageSrc: "/Categories/category3.png" },
+  { id: 4, imageSrc: "/Categories/category4.png" },
+  { id: 5, imageSrc: "/Categories/grocery.png" },
+  { id: 6, imageSrc: "/Categories/others.png" },
+];
 
 export default function Categories() {
   return (
-    <div className=" mt-10 text-font_color">
+    <div className="mt-10 text-font_color">
       <h2 className="font-bold text-2xl px-2 py-4 lg:text-left sm:text-center">
         Search different categories
       </h2>
       <div className="flex flex-row flex-wrap justify-center items-center">
-        <div className="p-2">
-          <Image
-            src={category1}
-            alt="category1"
-            width={200}
-            height={251}
-            className="rounded-lg"
-          />
-        </div>
-        <div className="p-2">
-          <Image
-            src={category2}
-            width={200}
-            height={251}
-            alt="category1"
-            className="rounded-lg"
-          />
-        </div>
-        <div className="p-2">
-          <Image
-            src={category3}
-            width={200}
-            height={251}
-            alt="category1"
-            className="rounded-lg"
-          />
-        </div>
-        <div className="p-2">
-          <Image
-            src={category4}
-            width={200}
-            height={251}
-            alt="category1"
-            className="rounded-lg"
-          />
-        </div>
-        <div className="p-2">
-          <Image
-            src={category5}
-            width={200}
-            height={251}
-            alt="category1"
-            className="rounded-lg"
-          />
-        </div>
-        <div className="p-2">
-          <Image
-            src={category6}
-            width={200}
-            height={251}
-            alt="category1"
-            className="rounded-lg"
-          />
-        </div>
+        {categories.map((category) => (
+          <div className="p-2" key={category.id}>
+            <Image
+              src={category.imageSrc}
+              alt={`category${category.id}`}
+              width={200}
+              height={251}
+              className="rounded-lg"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
--- components/category-feature/Feature.jsx
+++ components/category-feature/Feature.jsx
@@ -1,8 +1,34 @@
 import Image from "next/image";
-import smoothie from "public/features/smoothie.jpg";
-import drink from "public/features/drink.jpg";
-import orange from "public/features/orange.jpg";
-import timeIcon from "public/icon/time.svg";
+
+const promotions = [
+  {
+    imageSrc: "/features/orange.jpg",
+    date: "23 May - 26 May",
+    title: "ABC Restaurant Drink",
+    location: "Street 704, Phnom Penh",
+    originalPrice: "$10",
+    discountedPrice: "$5",
+    discountPercentage: "50% OFF",
+  },
+  {
+    imageSrc: "/features/drink.jpg",
+    date: "23 May - 26 May",
+    title: "XYZ Cafe Drink",
+    location: "Street 123, Phnom Penh",
+    originalPrice: "$8",
+    discountedPrice: "$4",
+    discountPercentage: "50% OFF",
+  },
+  {
+    imageSrc: "/features/smoothie.jpg",
+    date: "23 May - 26 May",
+    title: "Smoothie Paradise",
+    location: "Street 456, Phnom Penh",
+    originalPrice: "$12",
+    discountedPrice: "$6",
+    discountPercentage: "50% OFF",
+  },
+];
 
 export default function Feature() {
   return (
@@ -10,92 +36,44 @@ export default function Feature() {
       <h2 className="font-bold text-2xl px-2 py-4 lg:text-left sm:text-center">
         Hey, check out today&#39;s hottest promotions
       </h2>
-
       <div className="flex flex-row flex-wrap justify-center items-center">
-        <div className="p-2">
-          <Image
-            src={orange}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
-          <div className="flex flex-col flex-wrap pt-3">
-            <div className="flex flex-row">
-              <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
-              <p className="pl-2 text-base text-gray">23 May - 26 May</p>
-            </div>
-            <div className="text-lg font-bold">ABC Restaurant Drink</div>
-            <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1 items-center">
-              <div className="flex flex-row text-xl">
-                <div className="line-through">$10</div>
-                <div className="font-bold px-1">$5</div>
+        {promotions.map((promotion, index) => (
+          <div className="p-2" key={index}>
+            <Image
+              src={promotion.imageSrc}
+              width={418}
+              height={232}
+              alt="image"
+              className="rounded-md"
+            />
+            <div className="flex flex-col flex-wrap pt-3">
+              <div className="flex flex-row">
+                <Image
+                  src="/icon/time.svg"
+                  width={23}
+                  height={23}
+                  alt="timeIcon"
+                />
+                <p className="pl-2 text-base text-gray">{promotion.date}</p>
               </div>
-              <div className="pl-1">
-                <div className="bg-lightBlue text-xs text-blue p-1 rounded font-bold">
-                  50% OFF
+              <div className="text-lg font-bold">{promotion.title}</div>
+              <div className="text-base">{promotion.location}</div>
+              <div className="flex flex-row pt-1 items-center">
+                <div className="flex flex-row text-xl">
+                  <div className="line-through">{promotion.originalPrice}</div>
+                  <div className="font-bold px-1">
+                    {promotion.discountedPrice}
+                  </div>
+                </div>
+                <div className="pl-1">
+                  <div className="bg-lightBlue text-xs text-blue p-1 rounded font-bold">
+                    {promotion.discountPercentage}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="p-2">
-          <Image
-            src={drink}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
-          <div className="flex flex-col flex-wrap pt-3">
-            <div className="flex flex-row">
-              <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
-              <p className="pl-2 text-base text-gray">23 May - 26 May</p>
-            </div>
-            <div className="text-lg font-bold">ABC Restaurant Drink</div>
-            <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1 items-center">
-              <div className="flex flex-row text-xl">
-                <div className="line-through">$10</div>
-                <div className="font-bold px-1">$5</div>
-              </div>
-              <div className="pl-1">
-                <div className="bg-lightBlue text-xs text-blue p-1 rounded font-bold">
-                  50% OFF
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="p-2">
-          <Image
-            src={smoothie}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
-          <div className="flex flex-col flex-wrap pt-3">
-            <div className="flex flex-row">
-              <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
-              <p className="pl-2 text-base text-gray">23 May - 26 May</p>
-            </div>
-            <div className="text-lg font-bold">ABC Restaurant Drink</div>
-            <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1 items-center">
-              <div className="flex flex-row text-xl">
-                <div className="line-through">$10</div>
-                <div className="font-bold px-1">$5</div>
-              </div>
-              <div className="pl-1">
-                <div className="bg-lightBlue text-xs text-blue p-1 rounded font-bold">
-                  50% OFF
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
