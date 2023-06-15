4f2525c1fa3d8187d4c3159024cc74175dd989d9
DoungChan <sreoun.doungchan19@kit.edu.kh>
2023-06-15 15:01:59 +0700

    add RecoilRoot and atom

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
-
+import { RecoilRoot } from "recoil";
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <RecoilRoot>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
   );
 }
