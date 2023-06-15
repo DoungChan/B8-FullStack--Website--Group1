628ab18b86c9f1b882fe14fe2bc6f46ea43fb5fc
sivengo <sivengorn@gmail.com>
2023-06-15 16:59:26 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,10 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between `}
     >
-      <PromotionForm />
-      {/* <Feature />
+      <Feature />
       <Category />
 
-      <PopularPromotions /> */}
+      <PopularPromotions />
     </main>
   );
 }
